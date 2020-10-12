b8a0bb5e5b20e80ad317f71da25af390a7df3df7
Shannon Scirghio <shanscirg7@gmail.com>
2020-10-12 14:02:26 -0700

    added and styled footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./stylesheets/App.css";
 import Search from './components/Search';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Search />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className='footer-text'>2020 | <a className='website-link' href='https://shanscirg.com' target='_blank'>shanscirg.com</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
